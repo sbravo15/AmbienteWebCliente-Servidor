c7db43a7b1f588529b568c53483967e50c2f91ab
Santiago Bravo <67444457+sbravo15@users.noreply.github.com>
2022-04-06 01:01:52 -0600

    quitar (1)

---
+++ CodigoClases/Semana10/bd_viernes (1).sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-04-2022 a las 18:02:12
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_viernes`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarZapato` (IN `pcodigo` BIGINT, IN `pprecio` INT, IN `pobligatorio` INT, IN `popcional` INT)  BEGIN
+
+UPDATE zapato
+SET Precio = pprecio,
+	PorcObligatorio = pobligatorio,
+    PorcOpcional = popcional
+WHERE Codigo = pcodigo;
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarZapato` (IN `pCodigo` BIGINT)  BEGIN
+
+SELECT * FROM zapato
+WHERE codigo = pCodigo;
+
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ConsultarZapatos` ()  BEGIN
+
+SELECT * FROM zapato;
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `zapato`
+--
+
+CREATE TABLE `zapato` (
+  `Codigo` bigint(20) NOT NULL,
+  `Precio` decimal(12,2) NOT NULL,
+  `PorcObligatorio` int(11) NOT NULL,
+  `PorcOpcional` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `zapato`
+--
+
+INSERT INTO `zapato` (`Codigo`, `Precio`, `PorcObligatorio`, `PorcOpcional`) VALUES
+(1, '3500.00', 13, 1),
+(2, '1000.00', 5, 0),
+(4, '123.00', 4, 5),
+(5, '1000.00', 1, 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `zapato`
+--
+ALTER TABLE `zapato`
+  ADD PRIMARY KEY (`Codigo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `zapato`
+--
+ALTER TABLE `zapato`
+  MODIFY `Codigo` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
